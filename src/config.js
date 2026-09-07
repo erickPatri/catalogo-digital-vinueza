ea93fcbd755cf32586343b4e524e9a4aeaf1d6fd
erickPatri <erickvinueza11@gmail.com>
2026-09-07 10:17:31 -0500

    feat: add Accesorios category

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const CONFIG = {
   whatsappNumber: '593979961278',
   whatsappMessage: (productName) =>
     `Hola, estoy interesado en el producto: ${productName}. ¿Podrías darme más información?`,
-  categories: ['Navajas/Cuchillos', 'Chalecos', 'Linternas', 'Botas'],
+  categories: ['Navajas/Cuchillos', 'Chalecos', 'Linternas', 'Botas', 'Accesorios'],
 }
